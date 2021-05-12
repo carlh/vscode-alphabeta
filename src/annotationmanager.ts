37b9c3d86046667ba61531e4502a2c8f50fdb835
Carl Hinkle <cghinkle427@gmail.com>
2021-05-12 14:31:33 -0400

    Refactor the extension file out to annotationmanager and configurationmanager. Change the annotation styling.

---
+++ src/annotationmanager.ts
@@ -0,0 +1,170 @@
+import * as vscode from 'vscode';
+import * as ts from 'typescript';
+import { MarkdownString } from 'vscode';
+import {
+  onConfigurationUpdate,
+  refreshInterval,
+  showAlpha,
+  showAnnotations,
+  showBeta,
+  showInternal,
+} from './configurationmanager';
+
+const JSDOC_INTERNAL_ANNOTATION = '*@internal*';
+const JSDOC_ALPHA_ANNOTATION = '*@alpha*';
+const JSDOC_BETA_ANNOTATION = '*@beta*';
+
+const decorationType = vscode.window.createTextEditorDecorationType({
+  borderColor: 'rgb(145,0,13)',
+  backgroundColor: 'rgba(145,0,13,0.8)',
+  borderWidth: '2px',
+  borderRadius: '4px',
+});
+
+let timer: NodeJS.Timeout | null = null;
+
+const getIdentifierPositions = (
+  document: vscode.TextDocument
+): vscode.Position[] => {
+  const positions: vscode.Position[] = [];
+  let source: ts.SourceFile | undefined = ts.createSourceFile(
+    `${document.uri.fsPath}.tmp`,
+    document.getText(),
+    ts.ScriptTarget.Latest
+  );
+
+  const visit = (node: ts.Node): void => {
+    if (ts.isIdentifier(node)) {
+      positions.push(document.positionAt(node.end));
+    }
+    ts.forEachChild(node, visit);
+  };
+  if (source) {
+    ts.forEachChild(source, visit);
+  }
+  return positions;
+};
+
+const getHoverAnnotations = async (
+  document: vscode.TextDocument,
+  positions: vscode.Position[]
+): Promise<vscode.Hover[][]> => {
+  return Promise.all(
+    positions.map((position: vscode.Position): Thenable<vscode.Hover[]> => {
+      const thenable: Thenable<vscode.Hover[]> = vscode.commands.executeCommand(
+        'vscode.executeHoverProvider',
+        document.uri,
+        position
+      ) as Thenable<vscode.Hover[]>;
+      return thenable;
+    })
+  );
+};
+
+const containsMarkedRanges = (
+  hovers: vscode.Hover[],
+  token: string
+): boolean => {
+  return hovers.some((hover: vscode.Hover) => {
+    return hover.contents.some(
+      (content) => (content as MarkdownString)?.value.includes(token) ?? false
+    );
+  });
+};
+
+const containsBetaAnnotations = (hovers: vscode.Hover[]): boolean => {
+  if (!showBeta || !showAnnotations) {
+    return false;
+  }
+  return containsMarkedRanges(hovers, JSDOC_BETA_ANNOTATION);
+};
+
+const containsAlphaAnnotations = (hovers: vscode.Hover[]): boolean => {
+  if (!showAlpha || !showAnnotations) {
+    return false;
+  }
+  return containsMarkedRanges(hovers, JSDOC_ALPHA_ANNOTATION);
+};
+
+const containsInternalAnnotations = (hovers: vscode.Hover[]): boolean => {
+  if (!showInternal || !showAnnotations) {
+    return false;
+  }
+  return containsMarkedRanges(hovers, JSDOC_INTERNAL_ANNOTATION);
+};
+
+const getAnnotatedRanges = (hovers: vscode.Hover[][]): vscode.Range[] => {
+  return hovers.reduce((ranges: vscode.Range[], hover: vscode.Hover[]) => {
+    if (
+      containsInternalAnnotations(hover) ||
+      containsAlphaAnnotations(hover) ||
+      containsBetaAnnotations(hover)
+    ) {
+      return [...ranges, hover.pop()?.range as vscode.Range];
+    }
+    return ranges;
+  }, []);
+};
+
+const paintAnnotations = (
+  editor: vscode.TextEditor,
+  ranges: vscode.Range[],
+  decorationType: vscode.TextEditorDecorationType
+) => {
+  editor.setDecorations(decorationType, []);
+  editor.setDecorations(decorationType, ranges);
+};
+
+const onDidUpdateTextDocument = async (
+  document: vscode.TextDocument | undefined,
+  editor: vscode.TextEditor | undefined,
+  decorationType: vscode.TextEditorDecorationType
+) => {
+  if (editor && document) {
+    if (showAnnotations) {
+      const positions: vscode.Position[] = getIdentifierPositions(document);
+      const annotations: vscode.Hover[][] = await getHoverAnnotations(
+        document,
+        positions
+      );
+      const prerelease: vscode.Range[] = getAnnotatedRanges(annotations);
+      paintAnnotations(editor, prerelease, decorationType);
+    } else {
+      clearAnnotations();
+    }
+  }
+};
+
+export const updateAnnotations = () => {
+  if (!showAnnotations) {
+    clearAnnotations();
+    return;
+  }
+  if (timer !== null) {
+    clearInterval(timer);
+    timer = null;
+  }
+  vscode.window.visibleTextEditors.forEach((editor) => {
+    if (decorationType) {
+      onDidUpdateTextDocument(editor.document, editor, decorationType);
+    }
+  });
+  timer = setInterval(() => {
+    updateAnnotations();
+  }, refreshInterval * 1000);
+};
+
+const clearAnnotations = () => {
+  if (timer !== null) {
+    clearInterval(timer);
+    timer = null;
+  }
+
+  vscode.window.visibleTextEditors.forEach((editor) => {
+    if (decorationType) {
+      editor.setDecorations(decorationType, []);
+    }
+  });
+};
+
+onConfigurationUpdate.addListener(updateAnnotations);
---
+++ src/configurationmanager.ts
@@ -0,0 +1,55 @@
+import * as vscode from 'vscode';
+
+type ConfigurationUpdateHandler = () => void;
+
+class ConfigurationUpdateListener {
+  private _listeners: ConfigurationUpdateHandler[] = [];
+  readonly listeners = this._listeners;
+  addListener = (listener: ConfigurationUpdateHandler) => {
+    this._listeners.push(listener);
+  };
+}
+
+export const onConfigurationUpdate = new ConfigurationUpdateListener();
+
+export let showAnnotations: boolean = false;
+export let refreshInterval: number = 10;
+export let showInternal: boolean = false;
+export let showAlpha: boolean = false;
+export let showBeta: boolean = false;
+
+const debugConfiguration = () => {
+  console.log(`Show annotations: ${showAnnotations}`);
+  console.log(`Refresh interval: ${refreshInterval}`);
+  console.log(`Show internal: ${showInternal}`);
+  console.log(`Refresh alpha: ${showAlpha}`);
+  console.log(`Refresh beta: ${showBeta}`);
+};
+
+export const updateConfiguration = () => {
+  refreshInterval = vscode.workspace
+    .getConfiguration('AlphaBETA')
+    .get('control.refreshInterval') as number;
+
+  showAnnotations = vscode.workspace
+    .getConfiguration('AlphaBETA')
+    .get('control.showAnnotations') as boolean;
+
+  showInternal = vscode.workspace
+    .getConfiguration('AlphaBETA')
+    .get('phase.showInternal') as boolean;
+
+  showAlpha = vscode.workspace
+    .getConfiguration('AlphaBETA')
+    .get('phase.showAlpha') as boolean;
+
+  showBeta = vscode.workspace
+    .getConfiguration('AlphaBETA')
+    .get('phase.showBeta') as boolean;
+
+  onConfigurationUpdate.listeners.forEach((listener) => {
+    listener();
+  });
+
+  debugConfiguration();
+};
