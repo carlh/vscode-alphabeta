9a76dce73486a98d0a02b4bdd7cc7c8f72dab5c9
Carl Hinkle <cghinkle427@gmail.com>
2021-05-19 11:10:05 -0400

    Decrease annotation opacity to look better on dark background

--- src/decorations.ts
+++ src/decorations.ts
@@ -2,21 +2,21 @@ import { window } from 'vscode';
 
 export const internalDecorationType = window.createTextEditorDecorationType({
   borderColor: 'rgb(145,0,13)',
-  backgroundColor: 'rgba(145,0,13,0.8)',
+  backgroundColor: 'rgba(145,0,13,0.4)',
   borderWidth: '2px',
   borderRadius: '4px',
 });
 
 export const betaDecorationType = window.createTextEditorDecorationType({
   borderColor: 'rgb(18,166,75)',
-  backgroundColor: 'rgba(18,166,75,0.8)',
+  backgroundColor: 'rgba(18,166,75,0.4)',
   borderWidth: '2px',
   borderRadius: '4px',
 });
 
 export const alphaDecorationType = window.createTextEditorDecorationType({
   borderColor: 'rgb(192,188,23)',
-  backgroundColor: 'rgba(192,188,23,0.8)',
+  backgroundColor: 'rgba(192,188,23,0.4)',
   borderWidth: '2px',
   borderRadius: '4px',
 });
