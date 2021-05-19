538aa3e6d937b8edb791aaed762fc358680eae23
Carl Hinkle <cghinkle427@gmail.com>
2021-05-19 13:45:21 -0400

    Contribute the colors for the decorators so that users can customize them.

--- src/decorations.ts
+++ src/decorations.ts
@@ -1,22 +1,19 @@
-import { window } from 'vscode';
+import { window, ThemeColor } from 'vscode';
 
 export const internalDecorationType = window.createTextEditorDecorationType({
-  borderColor: 'rgb(145,0,13)',
-  backgroundColor: 'rgba(145,0,13,0.4)',
+  backgroundColor: { id: 'AlphaBETA.internal' },
   borderWidth: '2px',
   borderRadius: '4px',
 });
 
 export const betaDecorationType = window.createTextEditorDecorationType({
-  borderColor: 'rgb(18,166,75)',
-  backgroundColor: 'rgba(18,166,75,0.4)',
+  backgroundColor: { id: 'AlphaBETA.beta' },
   borderWidth: '2px',
   borderRadius: '4px',
 });
 
 export const alphaDecorationType = window.createTextEditorDecorationType({
-  borderColor: 'rgb(192,188,23)',
-  backgroundColor: 'rgba(192,188,23,0.4)',
+  backgroundColor: { id: 'AlphaBETA.alpha' },
   borderWidth: '2px',
   borderRadius: '4px',
 });
